5684107bab196975d114a006b9d7f6192edc021c
6StringCode <sixstringcode@gmail.com>
2022-05-31 14:25:41 -0400

    added VIrtual name and userLevel

---
+++ db/index.js
@@ -0,0 +1,67 @@
+const Sequelize = require('sequelize');
+const { ENUM, STRING, VIRTUAL } = Sequelize;
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_mail_db');
+
+const User = conn.define('user', {
+    firstName: {
+        type: STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    lastName: {
+        type: STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    fullName: {
+        type: VIRTUAL,
+        get: function(){
+            return `${this.firstName} ${this.lastName}`;
+        }
+    },
+    userLevel: {
+        type: ENUM('POWER', 'REGULAR', 'RESTRICTED'),
+        allowNull: false,
+        defaultValue: 'REGULAR'
+    }
+});
+
+const Message = conn.define('message', {
+    subject: {
+        type: STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+});
+
+const syncAndSeed = async() => {
+    await conn.sync({ force: true });
+    const [ moe, larry, lucy, ethyl ] = await Promise.all(
+        [
+            { firstName: 'moe', lastName: 'green', userLevel: 'POWER' },
+            { firstName: 'larry', lastName: 'lubin' },
+            { firstName: 'lucy', lastName: 'lasser', userLevel: 'RESTRICTED' },
+            { firstName: 'ethyl', lastName: 'evans' },
+        ].map( user => User.create(user))
+    );
+    const [ hi, bye, hello ] = await Promise.all(
+        [
+            { subject: 'hi' },
+            { subject: 'bye' },
+            { subject: 'hello' },
+        ].map( message => Message.create(message))
+    );
+}
+
+module.exports = {
+    conn,
+    User, 
+    Message,
+    syncAndSeed, 
+}
---
+++ server.js
@@ -0,0 +1,119 @@
+const db = require('./db');
+const { conn, User, Message, syncAndSeed } = db;
+const express = require('express');
+const app = express();
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', async(req, res, next)=> {
+    try {
+        const [ users, messages ] = await Promise.all([
+            User.findAll(),
+            Message.findAll(),
+        ]);
+        res.send(
+            `<html>
+                <head>
+                    <title>Acme Mail</title>
+                    <link rel='stylesheet' href='/assets/styles.css' />
+                </head>
+                <body>
+                    <nav>
+                        <a href='/' class='selected'>Home</a>
+                        <a href='/users'>Users</a>
+                        <a href='/messages'>Messages</a>
+                    </nav>
+                    <p>
+                        We have ${ users.length } users and we have ${ messages.length } messages!
+                    </p>
+                </body>
+            </html>`
+        )
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.get('/users', async(req, res, next)=> {
+    try {
+        const users = await User.findAll();
+        res.send(
+            `<html>
+                <head>
+                    <title>Acme Mail</title>
+                    <link rel='stylesheet' href='/assets/styles.css' />
+                </head>
+                <body>
+                    <nav>
+                        <a href='/'>Home</a>
+                        <a href='/users' class='selected'>Users</a>
+                        <a href='/messages'>Messages</a>
+                    </nav>
+                    <ul>
+                        ${ users.map( user => {
+                            return `
+                                <li>
+                                    ${ user.fullName } (${ user.userLevel })
+                                </li>
+                            `
+                        }).join('')
+                        }
+                    </ul>
+                </body>
+            </html>`
+        )
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.get('/messages', async(req, res, next)=> {
+    try {
+        const messages = await Message.findAll();
+        res.send(
+            `<html>
+                <head>
+                    <title>Acme Mail</title>
+                    <link rel='stylesheet' href='/assets/styles.css' />
+                </head>
+                <body>
+                    <nav>
+                        <a href='/'>Home</a>
+                        <a href='/users'>Users</a>
+                        <a href='/messages' class='selected'>Messages</a>
+                    </nav>
+                    <ul>
+                        ${ messages.map( message => {
+                            return `
+                                <li>
+                                    ${ message.subject }
+                                </li>
+                            `
+                        }).join('')
+                        }
+                    </ul>
+                </body>
+            </html>`
+        )
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+
+const bootstrap = async() => {
+    try {
+        await syncAndSeed();
+        console.log('seeded data');
+        const port = process.env.PORT || 3000;
+        app.listen(port, ()=> console.log(`listening on port ${port}`));
+    }
+    catch(ex){
+        console.log(ex);
+    }
+};
+
+bootstrap();
